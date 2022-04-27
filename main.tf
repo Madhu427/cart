e11524d6274f3a3eaf0b69742051927bfd47ce15
Madhu427 <madhusudhanachary.k@gmail.com>
2022-04-28 00:22:45 +0530

    cart_ami

---
+++ main.tf
@@ -0,0 +1,22 @@
+module "cart-ami" {
+  source = "git::https://github.com/Madhu427/terraform-ami.git"
+  COMPONENT      = "cart"
+  APP_VERSION    = var.APP_VERSION
+}
+
+variable APP_VERSION {}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+terraform {
+
+  backend "s3" {
+    bucket = "tf-bucket-61"
+    key    = "ami/cart/terraform.tfstate"
+    region = "us-east-1"
+  }
+
+}
